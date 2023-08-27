3a36d1d92262092c662ca2756f1ca1892ad08752
Montee' Ellis <129314929+Monteetechprep@users.noreply.github.com>
2023-08-27 18:00:46 +0000

    added a new gem and more get

--- dice.rb
+++ dice.rb
@@ -1,1 +1,15 @@
 require "sinatra"
+
+get("/") do
+  "Hello World"
+end
+
+get("/zebra") do
+  "We must add a route for each path we want to support"
+end
+
+require "sinatra/reloader"
+
+get("/giraffe") do
+  "Hopefully this shows up without having to restart the server 🤞🏾"
+end
