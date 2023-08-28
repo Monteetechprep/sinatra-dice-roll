b5c35fdf181787646780f365cf4313bc77190ae3
Montee' Ellis <129314929+Monteetechprep@users.noreply.github.com>
2023-08-28 21:09:40 +0000

    added more code to the erbs

--- dice.rb
+++ dice.rb
@@ -21,7 +21,7 @@ get("/dice/2/6") do
 
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  erb(:two_six)
+  erb(:two_six, { :layout => :wrapper })
 end
 
 get("/dice/2/10") do
@@ -31,7 +31,7 @@ get("/dice/2/10") do
 	
   @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-  erb(:two_ten)
+  erb(:two_ten, { :layout => :wrapper })
 end
 
 get("/dice/1/20") do
@@ -39,7 +39,7 @@ get("/dice/1/20") do
 	
   @outcome = "You rolled a #{first_die}."
 	
-  erb(:one_twenty)
+  erb(:one_twenty, { :layout => :wrapper })
 end
 
 get("/dice/5/4") do
@@ -51,7 +51,7 @@ get("/dice/5/4") do
 	
   @outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
 	
-  erb(:five_four)
+  erb(:five_four, { :layout => :wrapper })
 end
 require "better_errors"
 require "binding_of_caller"
